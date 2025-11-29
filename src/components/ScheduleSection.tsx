9e8c33a490126f217360974b725fbb0250c08344
Franz Noel <franz@tanglao.org>
2025-11-28 20:30:47 -0800

    feat: Add AM/PM to Scheduler

--- src/components/ScheduleSection.tsx
+++ src/components/ScheduleSection.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { Box, TextField, Typography, Stack } from '@mui/material';
 
 const hours = [
-  "6:00","7:00","8:00","9:00","10:00","11:00",
-  "12:00","1:00","2:00","3:00","4:00","5:00",
-  "6:00","7:00","8:00","9:00","10:00"
+  "6:00 AM","7:00 AM","8:00 AM","9:00 AM","10:00 AM","11:00 AM",
+  "12:00 PM","1:00 PM","2:00 PM","3:00 PM","4:00 PM","5:00 PM",
+  "6:00 PM","7:00 PM","8:00 PM","9:00 PM","10:00 PM"
 ];
 
 const ScheduleSection = () => (
@@ -15,7 +15,7 @@ const ScheduleSection = () => (
 
     {hours.map((h, index) => (
       <Stack direction="row" spacing={2} alignItems="center" key={`${h}-${index}`} sx={{ mb: 1 }}>
-        <Typography width={60}>{h}</Typography>
+        <Typography width={80} textAlign="right">{h}</Typography>
         <TextField fullWidth size="small" />
       </Stack>
     ))}
